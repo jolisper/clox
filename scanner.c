880aa180ee8742fd223e4805d6a12568e5e9fe34
Jorge Luis Pérez <jorge.perez@almundo.com>
2020-10-03 22:41:29 -0300

    Add lexical grammar basics (16.3 section)
    Support for, single and double characters tokens, whitespaces, comments
    and literal tokens.

--- scanner.c
+++ scanner.c
@@ -18,7 +18,34 @@ void initScanner(const char* source) {
   scanner.line = 1;
 }
 
+static bool isDigit(char c) {
+  return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd() { return *scanner.current == '\0'; }
+
+static char advance() {
+  scanner.current++;
+  return scanner.current[-1];
+}
+
+static char peek() {
+  return *scanner.current;
+}
+
+static char peekNext() {
+  if (isAtEnd()) return '\0';
+  return scanner.current[1];
+}
+
+static bool match(char expected) {
+  if (isAtEnd()) return false;
+  if (*scanner.current != expected) return false;
+
+  // current character match expected, advance current pointer:
+  scanner.current++;
+  return true;
+}
 
 static Token makeToken(TokenType type) {
   Token token;
@@ -40,10 +67,108 @@ static Token errorToken(const char* message) {
   return token;
 }
 
+static void skipWhitespace(){
+  for (;;) {
+    char c = peek();
+    switch(c) {
+    case ' ':
+    case '\r':
+    case '\t':
+      advance();
+      break;
+
+    case '\n':
+      scanner.line++;
+      advance();
+      break;
+
+    case '/':
+      if (peekNext() == '/') {
+        // A comment goes until the end of the line.
+        while (peek() != '\n' && !isAtEnd()) advance();
+      } else {
+        return;
+      }
+      break;
+
+    default:
+      return;
+    }
+  }
+}
+
+static Token number() {
+  while (isDigit(peek())) advance();
+
+  if (peek() == '.' && isDigit(peekNext())) {
+    // Consume the "."
+    advance();
+
+    while (isDigit(peek())) advance();
+  }
+
+  // Don’t convert the lexeme to a double yet
+  return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+  while(peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n') scanner.line++;
+    advance();
+  }
+
+  if (isAtEnd()) return errorToken("Unterminated string.");
+
+  // The closing quote.
+  advance();
+
+  // Instead of adding that complexity (to manage the memory for the string’s
+  // character array) to the scanner, we defer converting the literal lexeme to a
+  // runtime value until later. In clox, tokens only store the lexeme—the
+  // character sequence exactly as it appears in the user’s source code. Later
+  // in the compiler, we’ll convert that lexeme to a runtime value right when we
+  // are ready to store it in the chunk’s constant table.
+  return makeToken(TOKEN_STRING);
+}
+
 Token scanToken() {
+  skipWhitespace();
+
+  // Set start at the beginning of the token
   scanner.start = scanner.current;
 
   if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+  char c = advance();
+
+  // Instead of adding a switch case for each of the ten digits that can start a
+  // number, we handle them here:
+  if (isDigit(c)) return number();
+
+  switch(c) {
+  case '(': return makeToken(TOKEN_LEFT_PAREN);
+  case ')': return makeToken(TOKEN_RIGHT_PAREN);
+  case '{': return makeToken(TOKEN_LEFT_BRACE);
+  case '}': return makeToken(TOKEN_RIGHT_BRACE);
+  case ';': return makeToken(TOKEN_SEMICOLON);
+  case ',': return makeToken(TOKEN_COMMA);
+  case '.': return makeToken(TOKEN_DOT);
+  case '-': return makeToken(TOKEN_MINUS);
+  case '+': return makeToken(TOKEN_PLUS);
+  case '/': return makeToken(TOKEN_SLASH);
+  case '*': return makeToken(TOKEN_STAR);
+  // Two character tokens
+  case '!':
+    return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+  case '=':
+    return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+  case '<':
+    return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+  case '>':
+    return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  // Strings
+  case '"': return string();
+  }
 
   return errorToken("Unexpected character.");
 }
